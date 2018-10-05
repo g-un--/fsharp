333f8b8e071acead9a411eba8a76563dfa760e20
g-un-- <gino.ungureanu@uipath.com>
2018-10-05 16:43:39 +0300

    Solved: Allergies

--- exercises/allergies/Allergies.fs
+++ exercises/allergies/Allergies.fs
@@ -2,8 +2,25 @@
 
 open System
 
-// TODO: define the Allergen type
+[<Flags>]
+type Allergen = 
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
 
-let allergicTo codedAllergies allergen = failwith "You need to implement this function."
+let allergicTo (codedAllergies : int) (allergen : Allergen) = 
+    codedAllergies &&& (allergen |> int) >= 1
 
-let list codedAllergies = failwith "You need to implement this function."
+let list codedAllergies = 
+    Enum.GetValues(typeof<Allergen>) :?> (Allergen array)
+    |> Seq.ofArray 
+    |> Seq.where(fun x -> 
+        let res = codedAllergies &&& (x |> int) 
+        res >= 1)
+    |> List.ofSeq
+    |> List.map (fun id -> id)
